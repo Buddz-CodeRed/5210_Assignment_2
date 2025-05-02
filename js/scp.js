97ad20808fed12d2c972af9886a9fbfbbca8c830
Buddz-CodeRed <badi9code.red@gmail.com>
2025-05-02 13:28:14 +1200

    changed word limit

--- js/scp.js
+++ js/scp.js
@@ -68,7 +68,7 @@ let voices = [];
 
 // Function to read the scp with a word limit
 function readDescription(description) {
-    const maxWords = 40; // Set the word limit
+    const maxWords = 50; // Set the word limit
     const words = description.split(' ');
     const limitedDescription = words.slice(0, maxWords).join(' ');
 
